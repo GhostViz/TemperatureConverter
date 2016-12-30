e528725ab6f60fde0d5a17d60494c3254c96807f
GhostViz <thejoenichols@gmail.com>
2016-12-29 23:11:55 -0700

    Added a CLI menu

--- src/com/ghostviz/TempConversion.java
+++ src/com/ghostviz/TempConversion.java
@@ -1,13 +1,143 @@
 package com.ghostviz;
 
 
+import java.util.Scanner;
+
 public class TempConversion {
 
     public static void main(String[] args) {
+        conversionMenu();
+    }
 
+    public static void conversionMenu() {
+        System.out.println("What Conversion Would You Like to Do?");
+        System.out.print("1.)  Fahrenheit to Celsius   ");
+        System.out.print("2.) Fahrenheit to Kelvin  ");
+        System.out.println("3.) Fahrenheit to Rankine");
+        System.out.print("4.)  Celsius to Fahrenheit   ");
+        System.out.print("5.) Celsius to Kelvin     ");
+        System.out.println("6.) Celsius to Rankine");
+        System.out.print("7.)  Kelvin to Fahrenheit    ");
+        System.out.print("8.) Kelvin to Celsius     ");
+        System.out.println("9.) Kelvin to Rankine");
+        System.out.print("10.) Rankine to Fahrenheit  ");
+        System.out.print("11.) Rankine to Celsius   ");
+        System.out.println("12.) Rankine to Kelvin");
+
+        Scanner convChoice = new Scanner(System.in);
+        switch(convChoice.nextInt()) {
+            case 1:
+                System.out.print("Enter Fahrenheit: ");
+                Scanner tempScanner = new Scanner(System.in);
+                double temp = tempScanner.nextDouble();
+                System.out.println(temp + "°F is " + fahrenheitToCelsius(temp) + "°C\n");
+                tryAgain();
+                break;
+            case 2:
+                System.out.print("Enter Fahrenheit: ");
+                tempScanner = new Scanner(System.in);
+                temp = tempScanner.nextDouble();
+                System.out.println(temp + "°F is " + fahrenheitToKelvin(temp) + " K\n");
+                tryAgain();
+                break;
+            case 3:
+                System.out.print("Enter Fahrenheit: ");
+                tempScanner = new Scanner(System.in);
+                temp = tempScanner.nextDouble();
+                System.out.println(temp + "°F is " + fahrenheitToRankine(temp) + "°R\n");
+                tryAgain();
+                break;
+            case 4:
+                System.out.print("Enter Celsius: ");
+                tempScanner = new Scanner(System.in);
+                temp = tempScanner.nextDouble();
+                System.out.println(temp + "°C is " + celsiusToFahrenheit(temp) + "°F\n");
+                tryAgain();
+                break;
+            case 5:
+                System.out.print("Enter Celsius: ");
+                tempScanner = new Scanner(System.in);
+                temp = tempScanner.nextDouble();
+                System.out.println(temp + "°C is " + celsiusToKelvin(temp) + " K\n");
+                tryAgain();
+                break;
+            case 6:
+                System.out.print("Enter Celsius: ");
+                tempScanner = new Scanner(System.in);
+                temp = tempScanner.nextDouble();
+                System.out.println(temp + "°C is " + celsiusToRankine(temp) + "°R\n");
+                tryAgain();
+                break;
+            case 7:
+                System.out.print("Enter Kelvin: ");
+                tempScanner = new Scanner(System.in);
+                temp = tempScanner.nextDouble();
+                System.out.println(temp + "K is " + kelvinToFahrenheit(temp) + "°F\n");
+                tryAgain();
+                break;
+            case 8:
+                System.out.print("Enter Kelvin: ");
+                tempScanner = new Scanner(System.in);
+                temp = tempScanner.nextDouble();
+                System.out.println(temp + "K is " + kelvinToCelsius(temp) + "°C\n");
+                tryAgain();
+                break;
+            case 9:
+                System.out.print("Enter Kelvin: ");
+                tempScanner = new Scanner(System.in);
+                temp = tempScanner.nextDouble();
+                System.out.println(temp + "K is " + kelvinToRankine(temp) + "°R\n");
+                tryAgain();
+                break;
+            case 10:
+                System.out.print("Enter Rankine: ");
+                tempScanner = new Scanner(System.in);
+                temp = tempScanner.nextDouble();
+                System.out.println(temp + "°R is " + rankineToFahrenheit(temp) + "°F\n");
+                tryAgain();
+                break;
+            case 11:
+                System.out.print("Enter Rankine: ");
+                tempScanner = new Scanner(System.in);
+                temp = tempScanner.nextDouble();
+                System.out.println(temp + "°R is " + rankineToCelsius(temp) + "°C\n");
+                tryAgain();
+                break;
+            case 12:
+                System.out.print("Enter Rankine: ");
+                tempScanner = new Scanner(System.in);
+                temp = tempScanner.nextDouble();
+                System.out.println(temp + "°R is " + rankineToKelvin(temp) + " K\n");
+                tryAgain();
+                break;
+            default:
+                conversionMenu();
+                break;
+        }
+    }
+
+    public static void tryAgain() {
+        System.out.println("Would you like to do another conversion? (Y / N)");
+        Scanner tryAgain = new Scanner(System.in);
+        switch(tryAgain.next()) {
+            case "Y":
+                conversionMenu();
+                break;
+            case "N":
+                break;
+            case "y":
+                conversionMenu();
+                break;
+            case "n":
+                break;
+            default:
+                conversionMenu();
+                break;
+        }
     }
 
     public static double fahrenheitToCelsius(double fahrenheit) {
+
         double celsius = Math.round((((fahrenheit - 32) * 5)/9)*100d)/100d;
         return celsius;
     }
